dd640ba98ffcd0ac99fccf13cd8cad67080536df
Rajat Ambastha <rajatambastha0034@gmail.com>
2025-01-30 22:02:59 +0530

    Initial Commit

---
+++ src/main/java/com/spring/project/jwtimplementation/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.spring.project.jwtimplementation.repository;
+
+import com.spring.project.jwtimplementation.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
